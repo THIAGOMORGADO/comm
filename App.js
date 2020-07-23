ef6f75334e97959b12ea6adb2444289689414ea0
thiago morgado <thiago_morgado@hotmail.com>
2020-07-23 04:10:37 -0300

    Implementaçao de digital ok

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  View,
+  Modal,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  Alert,
+  Platform
+} from "react-native";
 
+import * as LocalAuthentication from "expo-local-authentication";
+import styles from './src/styles'
 export default function App() {
+  const [isModalVisible, setIsModalVisible] = useState(true);
+
+  async function authenticate() {
+    const hasPassword = await LocalAuthentication.hasHardwareAsync();
+
+    if (!hasPassword) return;
+
+    const { success, error } = await LocalAuthentication.authenticateAsync();
+
+    if (success) {
+      Alert.alert("Autenticação realizada com sucesso");
+    } else {
+      Alert.alert("A autenticação falhou. Por favor, digite sua senha!");
+    }
+
+    setIsModalVisible(false);
+  }
+
+  Platform.OS === "ios" && authenticate();
+
   return (
-    <View style={styles.container}>
-      <Text>Funcionando Perfeitamente</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        placeholderTextColor="#444"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        placeholderTextColor="#444"
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text>Entrar</Text>
+      </TouchableOpacity>
+
+      {Platform.OS === "android" && (
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={isModalVisible}
+          onShow={authenticate}
+        >
+          <View style={styles.modal}>
+            <Text style={styles.authText}>
+              Autentique-se utilizando sua digital
+            </Text>
+            <TouchableOpacity
+              onPress={() => {
+                LocalAuthentication.cancelAuthenticate();
+                setIsModalVisible(false);
+              }}
+            >
+              <Text style={styles.cancelText}>Cancelar</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      )}
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles.js
@@ -0,0 +1,51 @@
+import {StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#19181f",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  input: {
+    width: 200,
+    height: 45,
+    borderWidth: 2,
+    borderColor: "#7159c1",
+    borderRadius: 5,
+    marginBottom: 10,
+    padding: 10
+  },
+  button: {
+    width: 200,
+    height: 45,
+    borderWidth: 2,
+    borderColor: "#7159c1",
+    backgroundColor: "#7159c1",
+    borderRadius: 5,
+    marginBottom: 10,
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  modal: {
+    backgroundColor: "#333",
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    height: "40%"
+  },
+  cancelText: {
+    color: "red",
+    fontSize: 16
+  },
+  authText: {
+    color: "white",
+    fontSize: 16
+  }
+});
+
+export default styles;
